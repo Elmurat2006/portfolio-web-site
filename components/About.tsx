6297b67f5464b6bc9ab5e2175ee842f5d115b065
Elmurat <example@gmail.com>
2022-04-11 16:12:50 +0600

    commit

--- components/About.tsx
+++ components/About.tsx
@@ -1,27 +1,45 @@
 import React from 'react'
 
-import {GiRibbonMedal,GiDiploma,GiMedalSkull} from 'react-icons/gi'
+import { GiRibbonMedal, GiDiploma, GiMedalSkull } from 'react-icons/gi'
 const About = () => {
   return (
     <>
-    <h2 className="text-white text-4xl mt-16 mb-4">
-    Personal achievements
-  </h2>
-      <div className="flex justify-between items-center">
-   
-      <div className="w-96 h-full text-white rounded-xl" style = {{backgroundColor: '#2E0348'}}>
-        <h3 className="py-4  flex justify-center items-center text-2xl">Hackathon <BiMedal /></h3>
-        <p className="flex pb-4 justify-center items-center">Ogogo academy hackathon winner on UI/UX design</p>
+      <h2 className="mt-16 mb-4 text-4xl text-white">Personal achievements</h2>
+      <div className="flex items-center justify-between">
+        <div
+          className="h-full w-96 rounded-xl text-white"
+          style={{ backgroundColor: '#2E0348' }}
+        >
+          <h3 className="flex  items-center justify-center py-4 text-2xl">
+            Hackathon <GiMedalSkull />
+          </h3>
+          <p className="flex items-center justify-center pb-4">
+            Ogogo academy hackathon winner on UI/UX design
+          </p>
+        </div>
+        <div
+          className="h-full w-96 rounded-xl text-white"
+          style={{ backgroundColor: '#2E0348' }}
+        >
+          <h3 className="flex  items-center justify-center py-4 text-2xl">
+            Olympiad <GiRibbonMedal />
+          </h3>
+          <p className="flex items-center justify-center pb-4">
+            1st place in the Olympiad in Informatics
+          </p>
+        </div>
+        <div
+          className="h-full w-96 rounded-xl text-white"
+          style={{ backgroundColor: '#2E0348' }}
+        >
+          <h3 className="flex  items-center justify-center py-4 text-2xl">
+            Сourses <GiDiploma />
+          </h3>
+          <p className="flex items-center justify-center pb-4">
+            completed UI/UX design courses at ogogo academy
+          </p>
+        </div>
       </div>
-      <div className="w-96 h-full text-white rounded-xl" style = {{backgroundColor: '#2E0348'}}>
-        <h3 className="py-4  flex justify-center items-center text-2xl">Olympiad <GiRibbonMedal /></h3>
-        <p className="flex pb-4 justify-center items-center">1st place in the Olympiad in Informatics</p>
-      </div>
-      <div className="w-96 h-full text-white rounded-xl" style = {{backgroundColor: '#2E0348'}}>
-        <h3 className="py-4  flex justify-center items-center text-2xl">Сourses  <GiDiploma /></h3>
-        <p className="flex pb-4 justify-center items-center">completed UI/UX design courses at ogogo academy</p>
-      </div>
-    </div>
     </>
   )
 }
